a7c109dc5a346e072a10c60e1f1fda5e75453516
Chuck Driesler <cdriesler.iv@gmail.com>
2021-03-29 17:09:25 -0400

    create holes in floor if core voids exist

--- Floors/FloorsByLevels/src/FloorsByLevels.cs
+++ Floors/FloorsByLevels/src/FloorsByLevels.cs
@@ -16,6 +16,7 @@ namespace FloorsByLevels
         /// <returns>A FloorsByLevelsOutputs instance containing computed results and the model with any new elements.</returns>
         public static FloorsByLevelsOutputs Execute(Dictionary<string, Model> inputModels, FloorsByLevelsInputs input)
         {
+            // Exract level perimeters from model
             var levels = new List<LevelPerimeter>();
             inputModels.TryGetValue("Levels", out var model);
             if (model == null || model.AllElementsOfType<LevelPerimeter>().Count() == 0)
@@ -27,24 +28,27 @@ namespace FloorsByLevels
 
             levels.AddRange(model.AllElementsOfType<LevelPerimeter>());
 
+            // Extract any core voids in model if they exist
+            var voidProfiles = new List<Polygon>();
+            inputModels.TryGetValue("Core", out var coreModel);
+            if (coreModel != null)
+            {
+                var voidMasses = coreModel.AllElementsOfType<Mass>().Where(mass => mass.Name.ToLower().Contains("void"));
+                voidProfiles.AddRange(voidMasses.Select(mass => mass.Profile.Perimeter.Project(new Plane(Vector3.Origin, Vector3.ZAxis))));
+            }
+
+            // Begin generating cores
             var floors = new List<Floor>();
             var floorArea = 0.0;
             foreach (var level in levels)
             {
-                Floor floor = null;
-                var flrOffsets = level.Perimeter.Offset(input.FloorSetback * -1);
-                if (flrOffsets.Count() > 0)
-                {
-                    floor = new Floor(flrOffsets.First(), input.FloorThickness,
-                            new Transform(0.0, 0.0, level.Elevation - input.FloorThickness),
-                            floorMaterial, null, false, Guid.NewGuid(), null);
-                }
-                else
-                {
-                    floor = new Floor(level.Perimeter, input.FloorThickness,
-                            new Transform(0.0, 0.0, level.Elevation - input.FloorThickness),
-                            floorMaterial, null, false, Guid.NewGuid(), null);
-                }
+                var floorOffsets = level.Perimeter.Offset(input.FloorSetback * -1);
+                var floorProfile = new Profile(floorOffsets.FirstOrDefault() ?? level.Perimeter, voidProfiles, Guid.NewGuid(), "Level Profile");
+                var floorThickness = input.FloorThickness;
+                var floorBasis = new Transform(0.0, 0.0, level.Elevation - input.FloorThickness);
+
+                var floor = new Floor(floorProfile, floorThickness, floorBasis, floorMaterial, null, false, Guid.NewGuid(), null);
+
                 floors.Add(floor);
                 floorArea += floor.Area();
             }
